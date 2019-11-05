1ea4fca4dc511c4b23cf1079907fa3d3e10944ce
leonardo <alexandro_321@mail.ru>
2019-11-05 20:16:04 +1000

    Add project

---
+++ Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+    public void StartGame()
+    {
+        var canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        canvasGroup.interactable = false;
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+}
